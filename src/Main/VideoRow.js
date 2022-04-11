51cf6177705a0bc9a226fcc445285dc2a982f518
YashJagwani <yashjagwani2112@gmail.com>
2022-04-11 19:03:14 +0200

    base layout setup

---
+++ src/Main/VideoRow.js
@@ -0,0 +1,60 @@
+// import React from "react";
+// import Video from "./Video";
+
+// const VideoRow = (props) => {
+//   const { type, label, videos } = props;
+//   const renderRowLabel = () => {
+//     if (type === "normal") {
+//       return <h3 className="videos-row-label">{label}</h3>;
+//     }
+
+//     if (type === "channel") {
+//       return (
+//         <div className="videos-row-label-channel">
+//           <div className="left">
+//             <img src="" className="avatar" />
+//             <h3 className="video-row-label">Channel name</h3>
+//             <span className="small-text">Recommended channel for you</span>
+//           </div>
+//           <div className="right">
+//             <button className="subscribe-btn">Subscribe</button>
+//           </div>
+//         </div>
+//       );
+//     }
+//   };
+//   return (
+//     <div className="videos-row-container">
+//       <div className="row">
+//         <div className="video-label-container">{renderRowLabel()}</div>
+//         <div className="videos-row-item">
+//           {videos.map((video) => (
+//             <Video video={video} />
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default VideoRow;
+
+import React from "react";
+import "./VideoRow.css";
+
+const VideoRow = ({ views, description, timestamp, channel, title, image }) => {
+  return (
+    <div className="videorow">
+      <img src={image} alt="" />
+      <div className="videorow__text">
+        <h3>{title}</h3>
+        <p className="videorow__headline">
+          {channel} • {views} views • {timestamp}
+        </p>
+        <p className="videorow__description">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default VideoRow;
---
+++ src/Page/Home/HomeContent.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import VideoRow from "../../Main/VideoRow";
+// import RecommendedVideos from "../../Main/RecommendedVideos";
+import "./HomeContent.css";
+
+const HomeContent = () => {
+  return (
+    <div className="home-content">
+      {/* <RecommendedVideos /> */}this is home content
+    </div>
+  );
+};
+
+export default HomeContent;
